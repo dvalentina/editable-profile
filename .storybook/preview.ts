619737bb21e4a17acc8cbfab1174d17f26d560e3
Valentina Danilova <danilova.vv@phystech.edu>
2024-04-02 16:02:34 +0400

    feat: add theme support for storybook

--- .storybook/preview.ts
+++ .storybook/preview.ts
@@ -1,4 +1,10 @@
-import type { Preview } from "@storybook/react";
+import type { Preview } from '@storybook/react';
+
+import { ThemeProvider } from 'styled-components';
+import { withThemeFromJSXProvider } from '@storybook/addon-themes';
+
+import { lightTheme } from '../src/themes/lightTheme';
+import { darkTheme } from '../src/themes/darkTheme';
 
 const preview: Preview = {
   parameters: {
@@ -9,6 +15,17 @@ const preview: Preview = {
       },
     },
   },
+
+  decorators: [
+    withThemeFromJSXProvider({
+      themes: {
+        light: lightTheme,
+        dark: darkTheme,
+      },
+      defaultTheme: 'dark',
+      Provider: ThemeProvider,
+    }),
+  ],
 };
 
 export default preview;
