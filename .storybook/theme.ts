6bbf480e72c63aca9806c39e1c099145add261e9
Valentina Danilova <danilova.vv@phystech.edu>
2024-04-02 23:55:03 +0400

    feat: add logo and fonts to storybook

---
+++ .storybook/theme.ts
@@ -0,0 +1,10 @@
+import { create } from '@storybook/theming/create';
+
+export default create({
+  base: 'light',
+
+  // Brand
+  brandTitle: 'CompanyInvest',
+  brandUrl: 'https://github.com/dvalentina/editable-profile',
+  brandImage: '/logo.svg',
+});
